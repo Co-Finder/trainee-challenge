1b40630c85ed6d9733bf988007b865eaadfdf52b
Mario Gabriel <mario.contato40@gmail.com>
2022-01-25 19:42:57 -0300

    bug fixing, refactoring, login successfully

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,98 +1,78 @@
-import React, { useState } from "react";
-import {
-  SafeAreaView,
-  View,
-  Image,
-  TextInput,
-  ScrollView,
-  KeyboardAvoidingView,
-  TouchableOpacity,
-} from "react-native";
-import { Text, Input, Button, Icon } from "react-native-elements";
-import styles from "./styles";
-
-//   const loginFirebase = ()=>{
-//       firebase.auth().singhInEmailAndPassword(email, password)
-//       .then((userCredential) => {
-//         let user = userCredential.user;
-//         console.log(user);
-//         // navigation.navigate("Task", { idUser: user.id})
-//       })
-//       .catch((error) => {
-//         setErrorLogin(true);
-//         let errorCode = error.code;
-//         let errorMessage = error.message;
-//       });
-//   };
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, Image, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import firebase from '../../config/firebaseConfig';
+import styles from '../Login/styles';
 
 export default function Login({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorLogin, setErrorLogin] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const cameIn = () => {
+   const cameIn = () => {
     navigation.reset({
       index: 1,
       routes: [{ name: "Main" }],
     });
   };
-  
+
+
+  function loginFirebase(){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorCode, errorMessage);
+      
+    });
+  }
+
+  useEffect(()=>{
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+       console.log("Logado"+user.uid);
+       cameIn()
+      } else {
+        console.log('not logged in')
+      }
+    });
+  },[])
+
+  function logOutFirebase(){
+    firebase.auth().signOut().then(function() {
+      alert('successfully logged out')
+    }).catch(function(error) {
+      alert('Error"')
+    });
+  }
+
+  function createUserFirebase(){
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorCode, errorMessage);
+    });
+  }
 
   return (
     <KeyboardAvoidingView style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require("../../Img/Onboarding-logo.png")}
-      />
+      <Image style={styles.logo} source={require("../../Img/Onboarding-logo.png")} />
+      
       <Text style={styles.title}>Start Cooking</Text>
       <Text style={styles.subtitle}>Please enter your account here</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        Type="email-address"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        leftIcon={{ type: "font-awesome", name: "envelope" }}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        type="te"
-        value={password}
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-      />
-      {errorLogin === true ? (
-        <View style={styles.contentAlert}>
-          <MaterialCommunityIcons
-            name="alert-circle"
-            size={24}
-            color="#bdbdbd"
-          />
-          <Text style={styles.warningAlert}>Invalid Username os Password</Text>
-        </View>
-      ) : (
-        <View />
-      )}
+      <TextInput style={styles.input} placeholder='Username' onChangeText={email => setEmail(email)} value={email} leftIcon={{ type: "font-awesome", name: "envelope"}} />
 
-      {email === "" || password === "" ? (
-        <TouchableOpacity disabled={true} style={styles.buttonLogin}>
-          <Text style={styles.TextButtonLogin}>Login</Text>
-        </TouchableOpacity>
-      ) : (
-        <TouchableOpacity style={styles.buttonLogin}>
-          <Text style={styles.TextButtonLogin} onPress={() => cameIn()}>Login</Text>
-        </TouchableOpacity>
-      )}
-      <Text style={styles.registration}> dont't have a registration?</Text>
-      <Text
-        style={styles.linkSubscrybe}
-        onPress={() => navigation.navigate("NewUser")}
-      >
-        Subscribe now...
-      </Text>
-      <View style={{ height: 100 }} />
+      <TextInput style={styles.input} placeholder='Password' onChangeText={password => setPassword(password)} value={password} leftIcon={{ type: "font-awesome", name: "envelope"}}  />
+
+      <TouchableOpacity style={styles.button} onPress={()=>{ loginFirebase()}}>
+        <Text style={styles.TextButtonLogin}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={()=>{ logOutFirebase()}}>
+        <Text>LogOut</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={()=>{ createUserFirebase()}}>
+        <Text>NewUser</Text>
+      </TouchableOpacity>
     </KeyboardAvoidingView>
   );
 }
