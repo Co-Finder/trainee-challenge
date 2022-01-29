4d26115099eb19ee4b83033e6b7d50b62f799cd5
dennisjunior247 <dennisjunior247@gmail.com>
2022-01-29 12:20:16 +0100

    instlled navigation

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,6 +11,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+// @react-native-community/blur
+import com.cmcewen.blurview.BlurViewPackage;
 // react-native-safe-area-context
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 // react-native-screens
@@ -62,6 +64,7 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new BlurViewPackage(),
       new SafeAreaContextPackage(),
       new RNScreensPackage(),
       new VectorIconsPackage()
---
+++ src/screens/RecipeDetails/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const RecipeDetails = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default RecipeDetails;
+
+const styles = StyleSheet.create({});
